e7a62847c4b7250fc87ae4498f609ec011b6e64e
Kaycee Nuel <heykayceee@gmail.com>
2025-07-03 17:00:11 +0100

    feat: implemented the grpc server logic

--- prober-server/main.go
+++ prober-server/main.go
@@ -6,22 +6,54 @@ import (
 	"fmt"
 	"log"
 	"net"
+	"net/http"
 	"time"
+
+	pb "grpc-prober/prober"
 
 	"google.golang.org/grpc"
 )
 
 var (
-	port = flag.int("port", 50051, "the server port")
+	port = flag.Int("port", 50051, "the server port")
 )
 
 // the server is used to implment the proberService
 type server struct {
-	pb.UnImplementedProberServer
+	pb.UnimplementedProberServiceServer
 }
 
-func (s *server) DoProbes(ctx context.Context, in *pb.ProRequest) (*pb.ProbeReply, error) {
-	start = time.Now()
+func (s *server) DoProbes(ctx context.Context, in *pb.ProbeRequest) (*pb.ProbeReply, error) {
+	endpoint := in.GetEndpoint()
+	repetitions := in.GetRepetitions()
+
+	var total time.Duration
+	var successCount int
+
+	client := &http.Client{Timeout: 5 * time.Second}
+
+	for i := 0; i < int(repetitions); i++ {
+		start := time.Now()
+		resp, err := client.Get(endpoint)
+		if err != nil {
+			log.Printf("Request %d failed: %v", i+1, err)
+			continue
+		}
+		resp.Body.Close()
+		elapsed := time.Since(start)
+
+		total += elapsed
+		successCount++
+	}
+
+	var avgMs float32
+	if successCount > 0 {
+		avgMs = float32(total.Milliseconds()) / float32(successCount)
+	}
+
+	return &pb.ProbeReply{
+		LatencyMsecs: avgMs,
+	}, nil
 }
 
 func main() {
@@ -31,7 +63,7 @@ func main() {
 		log.Fatalf("Failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterProberServer(s, &server{})
+	pb.RegisterProberServiceServer(s, &server{})
 	log.Printf("server listening at %v", lis.Addr())
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to server: %v", err)
