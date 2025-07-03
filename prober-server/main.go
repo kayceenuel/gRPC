a21e8b3cdbc5fdf8651342d60d6f90dd21a7e59b
Kaycee Nuel <heykayceee@gmail.com>
2025-07-03 10:50:48 +0100

    add: server listener

--- prober-server/main.go
+++ prober-server/main.go
@@ -2,7 +2,13 @@ package main
 
 import (
 	"context"
+	"flag"
+	"fmt"
+	"log"
+	"net"
 	"time"
+
+	"google.golang.org/grpc"
 )
 
 var (
@@ -16,4 +22,18 @@ type server struct {
 
 func (s *server) DoProbes(ctx context.Context, in *pb.ProRequest) (*pb.ProbeReply, error) {
 	start = time.Now()
+}
+
+func main() {
+	flag.Parse()
+	lis, err := net.Listen("tcp", fmt.Sprintf(":%d", *port))
+	if err != nil {
+		log.Fatalf("Failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterProberServer(s, &server{})
+	log.Printf("server listening at %v", lis.Addr())
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to server: %v", err)
+	}
 }
