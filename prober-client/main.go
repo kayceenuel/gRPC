287f400a8a924f7011cbedfcfa46214c4e7e0b38
Kaycee Nuel <heykayceee@gmail.com>
2025-07-04 23:10:13 +0100

    implementing the grpc client

---
+++ prober-client/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"flag"
+	"log"
+
+	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials/insecure"
+)
+
+var (
+	serverAddr = flag.String("serverAddr", "localhost:50051", "the address to connec to")
+
+)
+
+func main() {
+	flag.Parse()
+	conn, err := grpc.Dial(*serverAddr, grpc.WithTransportCredentials(insecure.NewCredentials()))
+	if err != nil {
+		log.Fatalf("did not connect: %v", err)
+	}
+	defer.conn.Close()
+	c := pb.NewProberClient(conn)
+}
